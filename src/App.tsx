de46dcf1b6485084e67a9f33ab7f3f6157fbdc8f
impossiblephrase <khaerudin.sendi@gmail.com>
2024-09-26 23:24:37 +0900

    feat to do list exercise

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Chores ToDo List</h1>
+      <div>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Create guest experience mobile check in</p></h4>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Document current CI/CD progress</p></h4>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Perform code review for final Pillow-Talk release</p></h4>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Implement new color pallete from Design Team</p></h4>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Fix image uploading process for guest check in</p></h4>
+        <h4><p><input type="checkbox" name="" id="checkIn"/>Provide on-boarding documentation</p></h4>
+        <input type="text" name="" id="" />
+      </div>
+    </div>
+
+  )
+}
+
+export default App
